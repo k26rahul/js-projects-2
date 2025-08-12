2be03c0bacc2168ae1a595ea6b4691dff475dd85
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2025-08-12 12:41:20 +0530

    calculator done

--- calculator/main.js
+++ calculator/main.js
@@ -1,11 +1,43 @@
 let expression = '';
+let resultInput = document.querySelector('#result');
+let symbols = ['+', '-', '*', '/', '.'];
 
-function append(value) {
-  expression += value;
+function append(token) {
+  if (symbols.includes(token)) {
+    let last = expression[expression.length - 1];
+    if (symbols.includes(last)) {
+      if (token != '.' || last == '.') deleteLast();
+    }
+  }
+
+  expression += token;
+  resultInput.value = expression;
 }
 
 function displayResult() {
-  let result = eval(expression);
-  let resultDiv = document.querySelector('#result');
-  resultDiv.innerText = result;
+  if (expression == '') return;
+
+  let result;
+  try {
+    result = eval(expression);
+    if (result == Infinity) {
+      expression = '';
+    } else {
+      expression = String(result);
+    }
+  } catch {
+    result = 'ERROR!';
+    expression = '';
+  }
+  resultInput.value = result;
+}
+
+function allClear() {
+  expression = '';
+  resultInput.value = expression;
+}
+
+function deleteLast() {
+  expression = expression.slice(0, expression.length - 1);
+  resultInput.value = expression;
 }
