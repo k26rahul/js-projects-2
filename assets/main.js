f55a85db6c570d19d17e060e8c1e2a0953b4a318
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2025-10-08 01:05:21 +0530

    add_og_meta_tags

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
 let projectTemplate = document.querySelector('#project-template').innerHTML;
 
-fetch('./portfolio/projectsData.json')
+fetch('projects_data.json')
   .then(response => response.json())
   .then(data => {
     data.forEach(project => {
